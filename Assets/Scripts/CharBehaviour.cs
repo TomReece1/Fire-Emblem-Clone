bf7a73a828f9fa8a936e095c9da276f15ccf5551
TomReece1 <tomreece1@hotmail.co.uk>
2022-11-07 01:14:46 +0000

    made a method to find all possible routes but it crashes unity

--- Assets/Scripts/CharBehaviour.cs
+++ Assets/Scripts/CharBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
@@ -10,8 +11,8 @@ public class CharBehaviour : MonoBehaviour
 {
     private BoardTiles BoardTiles;
     private GameController GameController;
-    private int m = 20;
-    private int r = 3;
+    private int m = 9;
+    private int r = 2;
 
     public int hp = 100;
     private int dmg = 10;
@@ -31,19 +32,21 @@ public class CharBehaviour : MonoBehaviour
     {
         if (GameController.playerTurn)
         {
-            if (Input.GetKeyDown("s") && turnStage == 0) ShowTiles();
+            //if (Input.GetKeyDown("s") && turnStage == 0) ShowTiles();
             if (Input.GetKeyDown("m") && turnStage == 0) MoveMe();
             if (Input.GetKeyDown("a") && turnStage <= 1) Attack();
             if (Input.GetKeyDown("w") && turnStage <= 1) Wait();
         }
 
+        if (Input.GetKeyDown("r")) ListAllRoutePermsAsStrings();
+
     }
 
-    private void ShowTiles()
+    private void ShowTilesOld()
     {
         BoardTiles.ClearAllTiles();
-        ShowBlueTiles();
-        ShowRedTiles();
+        ShowBlueTilesOld();
+        ShowRedTilesOld();
     }
 
     private void Wait()
@@ -102,11 +105,11 @@ public class CharBehaviour : MonoBehaviour
             transform.position = hit.transform.position + new Vector3(0, 0.49f, 0);
             MoveAudioSource.Play();
             turnStage = 1;
-            ShowTiles();
+            ShowTilesOld();
         }
     }
 
-    private void ShowBlueTiles()
+    private void ShowBlueTilesOld()
     {
         Vector3 root = transform.position - new Vector3(0, 0.49f, 0);
 
@@ -133,7 +136,7 @@ public class CharBehaviour : MonoBehaviour
 
     }
 
-    private void ShowRedTiles()
+    private void ShowRedTilesOld()
     {
         Vector3 root = transform.position - new Vector3(0, 0.49f, 0);
 
@@ -181,5 +184,144 @@ public class CharBehaviour : MonoBehaviour
         }
     }
 
+    private void ShowBlueTiles()
+    {
+        //Make a list of coordinates that are blue tile candidates
+
+        Vector3 root = transform.position - new Vector3(0, 0.49f, 0);
+
+        List<Vector3> blueCandidates = new List<Vector3>();
+        blueCandidates.Add(root);
+
+        //at each point on the x axis from max moving left to max moving right
+        for (int i = -m; i <= m; i++)
+        {
+            //unused movement at this x is m-abs(i)
+            //bottom most blue is down by the amount of unused movement
+            //top most blue is up by the amount of unused movement
+            //from bottom to top are blue candidates
+            for (int j = -(m - Mathf.Abs(i)); j <= (m - Mathf.Abs(i)); j++)
+            {
+                blueCandidates.Add(root + new Vector3(i, 0, j));
+            }
+        }
+    }
+
+    private void ListAllRoutePerms(Vector3 start)
+    {
+        List<List<Vector3>> routes = new List<List<Vector3>>();
+
+        //loop i from 1 to m
+        //for i=1, add the following 4 lists of length 1 to routes:
+        //(0,-1), (0,1), (-1, 0), (1, 0)
+        //for i=2, add the following 16 lists of length 2 to routes:
+        //[(0,-1), (0,-2)], [(0,-1), (0,0)], [(0,-1), (-1,-1)], [(0,-1), (1,-1)]
+        //[(0,1), (0,0)], [(0,1), (0,2)], [(0,1), (-1,1)], [(0,1), (1,1)]
+        //[(-1,0), (-1,-1)], [(-1,0), (-1,1)], [(-1,0), (-2,0)], [(-1,0), (0,0)]
+        //[(1,0), (1,-1)], [(1,0), (1,1)], [(1,0), (0,0)], [(1,0), (2,0)]
+
+        for (int i = 1; i <= m; i++)
+        {
+            //Instantiate a single route
+
+            List<Vector3> route = new List<Vector3>();
+
+            //Add the route down
+            route.Add(start + new Vector3(0, 0, -1));
+
+
+
+        }
+    }
+
+    private List<string> ListAllRoutePermsAsStrings()
+    {
+        List<string> routes = new List<string>();
+
+        //Add these to routes
+        //D, U, L, R
+        //DD, DU, DL, DR
+        //UD, UU, UL, UR
+        //LD, LU, LL, LR
+        //RD, RU, RL, RR
+        //...up to length m
+
+        for (int j = 1; j <= Math.Pow(4, m); j++)
+        {
+            routes.Add("D");
+            routes.Add("U");
+            routes.Add("L");
+            routes.Add("R");
+
+        }
+        //Now we have D, U, L, R, D, U, L, R, D, U, L, R, D, U, L, R
+
+        int currIndex = 0;
+
+
+        //if m=3, we need to do the next step 4^1 times, then 4^0 times
+        //this is how many times we're going to repeat the cycle through D,U,L,R
+        for (int k = 1; k <= m-1; k++)
+        {
+
+        //double times = Math.Pow(4, (m-1-k));
+
+        for (int l = 1; l <= Math.Pow(4, (m - 1 - k)); l++)
+        {
+            //when k=1, append D 4^1 times, then U 4^1 times...
+            //then when k=0, append D 4^2 times, then U 4^2 times
+            for (int j = 1; j <= Math.Pow(4, k); j++)
+            {
+                routes[currIndex] += "D";
+                Debug.Log($"currIndex is {currIndex}, just made {routes[currIndex]}");
+                currIndex++;
+                
+            }
+            for (int j = 1; j <= Math.Pow(4, k); j++)
+            {
+                routes[currIndex] += "U";
+                Debug.Log($"currIndex is {currIndex}, just made {routes[currIndex]}");
+                currIndex++;
+            }
+            for (int j = 1; j <= Math.Pow(4, k); j++)
+            {
+                routes[currIndex] += "L";
+                Debug.Log($"currIndex is {currIndex}, just made {routes[currIndex]}");
+                currIndex++;
+            }
+            for (int j = 1; j <= Math.Pow(4, k); j++)
+            {
+                routes[currIndex] += "R";
+                Debug.Log($"currIndex is {currIndex}, just made {routes[currIndex]}");
+                currIndex++;
+            }
+        }
+        currIndex = 0;
+
+
+        }
+
+
+        for (int i = 0; i < Math.Pow(4, m); i++)
+        {
+            Debug.Log($"iternation {i} is {routes[i]}");
+        }
+
+        return routes;
+
+
+
+        
+    }
+
+    private void extendPathPerms(List<string> perms)
+    {
+
+    }
+
+    private void CheckCoordIfBlue(Vector3 start, Vector3 candidate)
+    {
+        //make a shortlist of routes from start to candidate
+    }
 
 }
